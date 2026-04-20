cc9552f691b9c1e90e9dde45f2fd967ae93d1066
Tim Jones <tim@timjones.io>
2026-04-20 21:45:40 +0100

    Bump NuGet packages

--- src/DotNetro.Compiler.Driver/Program.cs
+++ src/DotNetro.Compiler.Driver/Program.cs
@@ -1,23 +1,23 @@
 ﻿using System.CommandLine;
 using DotNetro.Compiler;
 
-var assemblyOption = new Option<FileInfo?>(name: "--assembly", description: "Path to the assembly to compile.");
-var outputOption = new Option<FileInfo?>(name: "--output", description: "Path to the output file.");
+var assemblyOption = new Option<FileInfo?>("--assembly") { Description = "Path to the assembly to compile." };
+var outputOption = new Option<FileInfo?>("--output") { Description = "Path to the output file." };
 
 var rootCommand = new RootCommand();
-rootCommand.AddOption(assemblyOption);
-rootCommand.AddOption(outputOption);
+rootCommand.Options.Add(assemblyOption);
+rootCommand.Options.Add(outputOption);
 
-rootCommand.SetHandler(
-    (assembly, output) =>
-    {
-        var compilationResult = CompilerDriver.Compile(assembly!.FullName, "Main");
-        File.WriteAllBytes(output!.FullName, [.. compilationResult.CompiledImage]);
+rootCommand.SetAction(parseResult =>
+{
+    var assembly = parseResult.GetValue(assemblyOption);
+    var output = parseResult.GetValue(outputOption);
 
-        File.WriteAllText(Path.ChangeExtension(output!.FullName, ".asm"), compilationResult.AssemblyCode);
-        File.WriteAllText(Path.ChangeExtension(output!.FullName, ".lst"), compilationResult.Listing);
-    }, 
-    assemblyOption, 
-    outputOption);
+    var compilationResult = CompilerDriver.Compile(assembly!.FullName, "Main");
+    File.WriteAllBytes(output!.FullName, [.. compilationResult.CompiledImage]);
 
-return await rootCommand.InvokeAsync(args);
+    File.WriteAllText(Path.ChangeExtension(output!.FullName, ".asm"), compilationResult.AssemblyCode);
+    File.WriteAllText(Path.ChangeExtension(output!.FullName, ".lst"), compilationResult.Listing);
+});
+
+return await rootCommand.Parse(args).InvokeAsync();
