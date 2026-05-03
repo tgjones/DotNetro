e73d86a07f74cd436ba41b5275665bdaf17891ee
Tim Jones <tim@timjones.io>
2026-05-03 22:03:04 +0100

    Add lit tests

--- src/Irie.Tools.Compiler/Program.cs
+++ src/Irie.Tools.Compiler/Program.cs
@@ -43,6 +43,9 @@ rootCommand.SetAction(parseResult =>
     passMgr.AddPass(new IRTranslatorPass(new MOS6502CallLowering()));
     passMgr.AddPass(new LegalizerPass(new MOS6502LegalizerInfo()));
     passMgr.AddPass(new InstructionSelectorPass(new MOS6502InstructionSelector()));
+    passMgr.AddPass(new PhiEliminationPass());
+    passMgr.AddPass(new RegisterAllocatorPass(new MOS6502RegisterInfo()));
+    passMgr.AddPass(new CopyEliminationPass());
     passMgr.Run(context);
 
     context.MachineModule.OpcodeNamer        = MOS6502InstructionInfo.GetDisplayName;
